9e268266197c5316562c51075cb33bf90018936b
Bernard Wong <ben.wc88@gmail.com>
2019-04-17 06:38:59 +0100

    Added BMI calculator tab.

---
+++ FitnessCompanion/FitnessCompanion/Views/BMICalculator.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FitnessCompanion
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BMICalculator : ContentPage
+	{
+		public BMICalculator ()
+		{
+			InitializeComponent ();
+            entH.Text = Util.currentUser.Height.ToString();
+            entWeight.Text = Util.currentUser.Weight.ToString();
+        }
+
+        private void BtnCalculate_Clicked(object sender, EventArgs e)
+        {
+            double height = Convert.ToDouble(entH.Text) / 100;
+            double weight = Convert.ToDouble(entWeight.Text);
+            double result = (weight / (height * height));
+
+            labelAns.Text = result.ToString("0.00");
+        }
+    }
+}
