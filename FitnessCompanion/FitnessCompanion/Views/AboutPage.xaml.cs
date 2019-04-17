4c87ef32b9ed4349ae5a082bbabfb5a11b622e11
Bernard Wong <ben.wc88@gmail.com>
2019-04-17 07:02:59 +0100

    About page

--- FitnessCompanion/FitnessCompanion/Views/AboutPage.xaml.cs
+++ FitnessCompanion/FitnessCompanion/Views/AboutPage.xaml.cs
@@ -15,6 +15,25 @@ namespace FitnessCompanion
 		public AboutPage ()
 		{
 			InitializeComponent ();
+            SetLabels();
 		}
+
+        public void SetLabels()
+        {
+            string description = "This is a Fitness Companion Application design to keep track " +
+                "of your personal nutrition values, your goal and how much intakes you have.";
+            lblDesc.Text = description;
+
+            string page1Desc = "Tracker Page: Add/Delete any intake you have and displayed onto the screen. " +
+                "It shows total nutrition you need and have.";
+            lblPage1.Text = page1Desc;
+
+            string page2Desc = "BMI Page: Calculate the BMI by inputting height and weight. The default value" +
+                "for the two parameters are taken from user's account own height and weight.";
+            lblPage2.Text = page2Desc;
+
+            string page3Desc = "Account Page: Where you can modify your nutrition goals and weight and height.";
+            lblPage3.Text = page3Desc;
+        }
 	}
 }
--- FitnessCompanion/FitnessCompanion/obj/Debug/netstandard2.0/Views/AboutPage.xaml.g.cs
+++ FitnessCompanion/FitnessCompanion/obj/Debug/netstandard2.0/Views/AboutPage.xaml.g.cs
@@ -17,8 +17,28 @@ namespace FitnessCompanion {
     public partial class AboutPage : global::Xamarin.Forms.ContentPage {
         
         [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Grid contentGrid;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label lblDesc;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label lblPage1;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label lblPage2;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
+        private global::Xamarin.Forms.Label lblPage3;
+        
+        [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Forms.Build.Tasks.XamlG", "0.0.0.0")]
         private void InitializeComponent() {
             global::Xamarin.Forms.Xaml.Extensions.LoadFromXaml(this, typeof(AboutPage));
+            contentGrid = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Grid>(this, "contentGrid");
+            lblDesc = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lblDesc");
+            lblPage1 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lblPage1");
+            lblPage2 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lblPage2");
+            lblPage3 = global::Xamarin.Forms.NameScopeExtensions.FindByName<global::Xamarin.Forms.Label>(this, "lblPage3");
         }
     }
 }
